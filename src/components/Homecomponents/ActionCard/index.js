2f1764efc0fba57b53332a489b8c6ed7493dad55
Ilqar03 <tu757r7k9@code.edu.az>
2022-12-21 13:39:55 +0400

    fad

--- src/components/Homecomponents/ActionCard/index.js
+++ src/components/Homecomponents/ActionCard/index.js
@@ -2,7 +2,27 @@ import React from 'react'
 import "./index.scss"
 function ActionCard() {
   return (
-    <div id='ActionCard'>ActionCard</div>
+    <div id='ActionCard'>
+      <h2>ROG IN ACTION</h2>
+      <div id='card'>
+        <div>
+          <img src='https://dlcdnrog.asus.com/rog/media/1671059676670.webp'></img>
+          <p>After one year with Xbox Game Pass, I'm never giving it up</p>
+          <p>I once dreamed of a day when I could play any game I wanted, whenever I wanted, with the push of a button. Xbox Game Pass made that dream a reality. </p>
+        </div>
+        <div>
+          <img src='https://dlcdnrog.asus.com/rog/media/1664418290661.webp'></img>
+          <p>Star Citizen: 10 years of pushing the boundaries of what a game can be</p>
+          <p>Star Citizen is a space simulation game that has spent a decade redefining what is possible in the genre.</p>
+        </div>
+        <div>
+          <img src='https://dlcdnrog.asus.com/rog/media/1652837729215.webp'></img>
+          <p>Take an incredible journey across the Old West in Red Dead Redemption 2</p>
+          <p>Travel back in time to the Old West with this story-driven masterpiece.</p>
+        </div>
+      </div>
+      <a href=''>Learn More > </a>
+    </div>
   )
 }
 
--- src/components/Homecomponents/UniverseSec/index.js
+++ src/components/Homecomponents/UniverseSec/index.js
@@ -2,7 +2,19 @@ import React from 'react'
 import "./index.scss"
 function UniverseSec() {
   return (
-    <div id='UniverseSec'>UniverseSec</div>
+    <div id='UniverseSec'>
+      <div id='img'>
+      <div>
+      <h2>THE ART OF THE ROG UNIVERSE</h2>
+      <p>Technology never sleeps, nor should the action. For those keen on winning power, the Republic of Gamers awaits. Driven by relentless innovation, ROG is committed to producing out-of-this-world experiences for gamers and enthusiasts everywhere.</p>
+      </div>
+      </div>
+      <div id='res_img'>
+          <h2>THE ART OF THE ROG UNIVERSE</h2>
+          <p>Technology never sleeps, nor should the action. For those keen on winning power, the Republic of Gamers awaits. Driven by relentless innovation, ROG is committed to producing out-of-this-world experiences for gamers and enthusiasts everywhere.</p>
+          <img src='https://dlcdnwebimgs.asus.com/gain/C2447980-FC01-41FD-B062-184649D61350'></img>
+        </div>
+    </div>
   )
 }
 
